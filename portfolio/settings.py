b13957192e5e6d55195240850db6b8e4f83ca6dc
Amulya Jain <jainamulyawin@gmail.com>
2024-12-09 15:19:47 +0530

    add codes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t1=xn(98#odr6zksk6*-av2*j*)t(_o0k1i^1y$!2v9!re6)ow"
+# SECRET_KEY = "django-insecure-t1=xn(98#odr6zksk6*-av2*j*)t(_o0k1i^1y$!2v9!re6)ow"
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
